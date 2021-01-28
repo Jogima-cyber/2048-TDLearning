9582ec29ae0552533bc2a1f507c3d66a15c11695
Joseph Amigo <joseph.amigo@hec.edu>
2021-01-28 12:45:11 +0100

    Update application.js

--- js/application.js
+++ js/application.js
@@ -1,4 +1,12 @@
 // Wait till the browser is ready to render the game (avoids glitches)
 window.requestAnimationFrame(function () {
-  new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager);
+  $.ajax({
+    url: './weights/TDL2048_weights.json',
+    async: false,
+    dataType: 'json',
+    success: function (response) {
+      // do stuff with response.
+      new GameManager(4, KeyboardInputManager, HTMLActuator, LocalStorageManager, response);
+    }
+  });
 });
